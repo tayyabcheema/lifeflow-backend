a7723177f17f959a798fee0df1ad6efb55f9713c
Tayyab Cheema <ali.tayyab.cheema@gmail.com>
2024-09-14 14:36:00 +0500

    bug fixed 2.0

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,16 +1,18 @@
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Mongodb connected successfully");
   } catch (error) {
     console.log(error);
     console.log("Error connecting to database");
   }
-
 };
 
 module.exports = connectDB;
