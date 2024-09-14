f4a2bed7750ff0d36156ff9ceabfebceb1d21bd0
Tayyab Cheema <ali.tayyab.cheema@gmail.com>
2024-09-14 14:30:12 +0500

    code updated

--- config/connectDB.js
+++ config/connectDB.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI.toString());
     console.log("Mongodb connected successfully");
   } catch (error) {
     console.log(error);
