3c2d09110bd611229b1c95a1de5c74b08de8a1b2
Tayyab Cheema <ali.tayyab.cheema@gmail.com>
2024-09-14 14:32:54 +0500

    bug fixed

--- config/connectDB.js
+++ config/connectDB.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI.toString());
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Mongodb connected successfully");
   } catch (error) {
     console.log(error);
