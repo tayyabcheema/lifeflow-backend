4bc455379e7f87f826b40c5bdd304c78f059ade8
Tayyab Cheema <ali.tayyab.cheema@gmail.com>
2024-09-15 09:01:57 +0500

    testing

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,10 +1,11 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const mongoose = require("mongoose");
+const MONGO_URI = process.env.MONGO_URI
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://tayyabcheema:tayyab007@cluster0.2wsh1um.mongodb.net/lifeflow?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect(MONGO_URI);
     console.log("Mongodb connected successfully");
   } catch (error) {
     console.log(error);
