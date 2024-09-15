824fbdd79de89a442dfaaa7e4af77715d38f4ff0
Tayyab Cheema <ali.tayyab.cheema@gmail.com>
2024-09-15 08:55:44 +0500

    fixed

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,12 +1,10 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const mongoose = require("mongoose");
-const MONGO_URL = process.env.MONGO_URI
 
 const connectDB = async () => {
   try {
-    console.log("MONGO_URL = ", MONGO_URL.toString());
-    await mongoose.connect(MONGO_URL.toString());
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Mongodb connected successfully");
   } catch (error) {
     console.log(error);
